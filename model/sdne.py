0c6892ea78302d2131a3d96ae513a7a6f338f1ce
zhangweiguo <zhangweiguo717@163.com>
2018-10-16 01:24:20 +0800

    epoch end

--- model/sdne.py
+++ model/sdne.py
@@ -222,14 +222,17 @@ class SDNE:
                 loss = 0
                 embedding = None
                 while not graph.epoch_end:
-                    mini_batch = graph.sample(batch_size)
                     start = graph.start
+                    mini_batch = graph.sample(batch_size)
                     loss = self.fit(mini_batch)
                     self.logger("SDNE Epoch %3d Error: %5d/%5d %5.6s"%(current_epoch, start, graph.node_number, loss))
                 model_path_epoch = model_path + ".%s"%str(current_epoch)
                 embedding_path_epoch = embedding_path + ".%s"%str(current_epoch)
                 self.save_model(model_path_epoch)
 
+                loss = 0
+                mini_batch = graph.sample(batch_size, shuffle=False)
+                loss += self.get_loss(mini_batch)
                 while not graph.epoch_end:
                     mini_batch = graph.sample(batch_size, shuffle=False)
                     loss += self.get_loss(mini_batch)
